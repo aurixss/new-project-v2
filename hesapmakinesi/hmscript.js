f6dfd95f8950924ffc63ba19dd5758b476cac5cb
áurix <88969922+aurixss@users.noreply.github.com>
2022-07-14 21:38:37 +0300

    Add files via upload

---
+++ hesapmakinesi/hmscript.js
@@ -0,0 +1,28 @@
+
+  function Toplama(){
+	  var s1=Number(document.getElementById("sayi1").value);
+	  var s2=Number(document.getElementById("sayi2").value);
+	  var sonuc=s1+s2;
+	  document.getElementById("kutuSonuc").innerHTML="Sonuç: "+sonuc;
+  }
+  
+  function Cikarma(){
+	  var s1=Number(document.getElementById("sayi1").value);
+	  var s2=Number(document.getElementById("sayi2").value);
+	  var sonuc=s1-s2;
+	  document.getElementById("kutuSonuc").innerHTML="Sonuç: "+sonuc;
+  }
+  
+  function Carpma(){
+	  var s1=Number(document.getElementById("sayi1").value);
+	  var s2=Number(document.getElementById("sayi2").value);
+	  var sonuc=s1*s2;
+	  document.getElementById("kutuSonuc").innerHTML="Sonuç: "+sonuc;
+  }
+  
+  function Bolme(){
+	  var s1=Number(document.getElementById("sayi1").value);
+	  var s2=Number(document.getElementById("sayi2").value);
+	  var sonuc=s1/s2;
+	  document.getElementById("kutuSonuc").innerHTML = "Sonuç: "+sonuc;
+  }
